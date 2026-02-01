ec89382ec23114d829282d0837332288b390a053
Sergei Plekhov <sergei.plekhov@telekom.com>
2026-02-01 11:42:08 +0200

    added bash alias example

--- cal3m.py
+++ cal3m.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
+
+# $ export PATH=$PATH:/Users/A97066922/magenta/tesseract
+# $ cal() {
+# >     # If "-3m" is anywhere in the arguments, use cal3m.py
+# >     for arg in "$@"; do
+# >         if [ "$arg" = "-3m" ]; then
+# >             command cal3m.py "$@"
+# >             return
+# >         fi
+# >     done
+# > 
+# >     # Otherwise call the real macOS cal
+# >     command /usr/bin/cal "$@"
+# > }
+# $ 
+# $ cal
+#    February 2026      
+# Su Mo Tu We Th Fr Sa  
+#  1  2  3  4  5  6  7  
+#  8  9 10 11 12 13 14  
+# 15 16 17 18 19 20 21  
+# 22 23 24 25 26 27 28  
+                      
+                      
+# $ cal -3m
+#     January 2026         February 2026           March 2026     
+# Mo Tu We Th Fr Sa Su  Mo Tu We Th Fr Sa Su  Mo Tu We Th Fr Sa Su
+#           1  2  3  4                     1                     1
+#  5  6  7  8  9 10 11   2  3  4  5  6  7  8   2  3  4  5  6  7  8
+# 12 13 14 15 16 17 18   9 10 11 12 13 14 15   9 10 11 12 13 14 15
+# 19 20 21 22 23 24 25  16 17 18 19 20 21 22  16 17 18 19 20 21 22
+# 26 27 28 29 30 31     23 24 25 26 27 28     23 24 25 26 27 28 29
+#                                             30 31               
+# $ 
+
 import datetime
 import calendar
 
